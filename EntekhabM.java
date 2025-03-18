4cc83700684c50152189a0d1317a546b0fdee435
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-03-18 12:52:10 +0330

    choose

---
+++ EntekhabM.java
@@ -0,0 +1,267 @@
+public class EntekhabM {
+    int behdashti;
+
+    public void behdashtisabad(int a1) {
+        switch (a1) {
+            case 1:
+                System.out.println("tayd");
+                break;
+            case 2:
+                System.out.println("rika");
+                break;
+            case 3:
+                System.out.println("maye zarf shoyee");
+                break;
+            case 4:
+                System.out.println("vaytex");
+                break;
+
+            default:
+                System.out.println("not in renj");
+
+        }
+    }
+    public String beh(int b1){
+       if (b1 == 1){
+           return ("tayd");
+        } else if (b1 == 2) {
+           return ("rika");
+       } else if (b1 == 3) {
+           return ("maye-zarf-shoyee");
+       } else if (b1 == 4) {
+           return ("vaytex");
+       } else
+           return ("not in ranj");
+    }
+    public void shoyandesabad(int a1) {
+        switch (a1) {
+            case 1:
+                System.out.println("shampoo");
+                break;
+
+            case 2:
+                System.out.println("saboon");
+                break;
+
+            case 3:
+                System.out.println("body splash");
+                break;
+
+            case 4:
+                System.out.println("lif");
+                break;
+
+            default:
+                System.out.println("not in renj");
+
+        }
+    }
+
+    public String sho(int b1){
+        if (b1 == 1){
+            return ("shampoo");
+        } else if (b1 == 2) {
+            return ("saboon");
+        } else if (b1 == 3) {
+            return ("body splash");
+        } else if (b1 == 4) {
+            return ("lif");
+        }else {
+            return ("not in ranj");
+        }
+    }
+    public void ghazaiisabad(int a1) {
+        switch (a1) {
+            case 1 :
+            System.out.println("makaroonni");
+            break;
+
+            case 2 :
+            System.out.println("rashte soop");
+            break;
+
+            case 3 :
+                System.out.println("reshte ash");
+                break;
+
+            case 4 :
+                System.out.println("rob");
+                break;
+
+            default:
+                System.out.println("not in renj");
+
+        }
+    }
+    public String ghaz(int b1){
+        if (b1 == 1){
+            return ("makarooni");
+        } else if (b1 == 2) {
+            return ("reshte soop");
+        } else if (b1 == 3) {
+            return ("reshte ash ");
+        }else if (b1 == 4){
+            return ("rob");
+        }else
+            return ("not in ranj");
+    }
+    public void noshidanisabad(int a1) {
+      switch (a1){
+          case 1 :
+            System.out.println("ab porteghal");
+            break;
+
+          case 2 :
+            System.out.println("ab havij");
+            break;
+
+          case 3 :
+        System.out.println("ab anar ");
+        break;
+
+          case 4 :
+        System.out.println("ab sib");
+        break;
+
+
+          default:
+              System.out.println("not in renj");
+      }
+    }
+    public String nosh(int b1){
+        if (b1 == 1){
+            return ("ab-porteghal");
+        } else if (b1 == 2) {
+            return ("ab-havij");
+        } else if (b1 == 3) {
+            return ("ab-anar");
+        } else if (b1 == 4) {
+            return ("ab-sib");
+        }else
+            return ("not in ranj ");
+    }
+    public void gooshtisabad(int a1){
+        switch (a1){
+            case 1 :
+            System.out.println("morgh");
+            break;
+
+            case 2 :
+            System.out.println("goosfandi");
+            break;
+
+            case 3 :
+            System.out.println("gav");
+            break;
+
+            case 4 :
+        System.out.println("goosale");
+        break;
+
+            default:
+                System.out.println("not in renj");
+
+        }
+    }
+    public String goosht(int b1){
+        if (b1 == 1 ){
+            return("moorgh");
+        } else if (b1 == 2) {
+            return ("goosfandi");
+        } else if (b1 == 3) {
+            return ("goosale");
+        } else if (b1 == 4) {
+            return ("gav");
+        }else
+            return ("not in ranj ");
+    }
+    public void sabzijatsabad(int a1){
+        switch (a1){
+            case 1 :
+            System.out.println("reyhoon");
+            break;
+
+            case 2 :
+            System.out.println("jafari");
+            break;
+
+            case 3 :
+            System.out.println("piazche");
+            break;
+
+            case 4 :
+           System.out.println("nana");
+           break;
+
+            case 5 :
+            System.out.println("avishan");
+            break;
+
+            default:
+            System.out.println("not in renj");
+        }
+    }
+    public String sabz(int b1){
+        if (b1 == 1){
+            return ("reyhoon");
+        } else if (b1 == 2) {
+            return ("jafari");
+        } else if (b1 == 3) {
+            return ("piazche");
+        } else if (b1 == 4) {
+            return ("nanu");
+        } else if (b1 == 5) {
+            return ("avishan");
+        }else
+            return ("not in ranj");
+    }
+    public void mivejatsabad(int a1){
+        switch (a1){
+            case 1 :
+            System.out.println("sib");
+            break;
+
+            case 2 :
+            System.out.println("porteghal");
+            break;
+
+            case 3 :
+            System.out.println("holoo");
+            break;
+
+            case 4 :
+          System.out.println("zardaloo");
+          break;
+
+            case 5  :
+                System.out.println("mooz");
+                break;
+
+            case 6 :
+                System.out.println("gilas");
+                break;
+
+
+            default:
+                System.out.println("not in renj");
+        }
+    }
+    public String mive(int b1 ){
+        if (b1 == 1){
+            return ("sib");
+        } else if (b1 == 2) {
+            return ("porteghal");
+        }else if (b1 == 3){
+            return ("holoo");
+        } else if (b1 == 4) {
+            return ("zardaloo");
+        } else if (b1 == 5) {
+            return ("mooz");
+        } else if (b1 == 6) {
+            return ("gilass");
+        } else if (b1 == 7) {
+            return ("hendoone");
+        }else
+            return ("not in ranj");
+    }
+}
