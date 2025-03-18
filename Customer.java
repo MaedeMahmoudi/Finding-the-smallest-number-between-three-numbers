7e91c3c68a409c22bd314b4c9ad4c60da40e36ac
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-03-18 12:49:43 +0330

    Customer Informations

---
+++ Customer.java
@@ -0,0 +1,45 @@
+public class Customer {
+    String name;
+    String lastname;
+    int number;
+    kala kala ;
+    FactorForosh factorforosh;
+    public Customer(){
+        this.name = name;
+        this.lastname = lastname;
+        this.number = number;
+        this.kala = kala;
+        this.factorforosh = factorforosh;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getLastname(){
+        return lastname;
+    }
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+    public int getNumber(){
+        return number;
+    }
+    public void setNumber(int number){
+        this.number = number;
+    }
+    public kala getKala(){
+        return kala;
+    }
+    public void setKala(kala kala){
+        this.kala = kala;
+    }
+
+    public FactorForosh getFactorforosh() {
+        return factorforosh;
+    }
+    public void setFactorforosh(FactorForosh factorforosh){
+        this.factorforosh = factorforosh;
+    }
+}
