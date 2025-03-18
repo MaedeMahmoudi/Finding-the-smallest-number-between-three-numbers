cfb6d1284b74863e0ca950255039ac84942fa59e
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-03-18 13:02:44 +0330

    Price Managment

---
+++ Manager.java
@@ -0,0 +1,80 @@
+public class Manager {
+
+    private String nameshoyandeh;
+    private String namebedashti;
+    private String nameghazayii;
+    private String namenoshidani;
+    private String namegooshti;
+    private String namesabzijat;
+    private String namemivejat;
+
+
+
+    public void nameshoyandeh1() {
+        System.out.println("tayd" + '\n' + "rika " + '\n' + "mayee zarf-shoyee" + '\n' + "vaytex ");
+    }
+    public void namebedashti(){
+        System.out.println("shampoo"+'\n'+"saboon"+'\n'+"body splash"+'\n'+"lif"+'\n'+"dastmal-kaghazi");
+    }
+    public void nameghazayii(){
+        System.out.println("makarani"+'\n'+"reshte-soop"+'\n'+"rashte-ash"+'\n'+"rob"+'\n'+"berenj");
+    }
+    public void namegooshti(){
+      System.out.println("morgh"+'\n'+"gooosfandi"+'\n'+"gav"+'\n'+"goosale");
+    }
+    public void namenoshidani(){
+        System.out.println("ab-porteghal"+'\n'+"ab-havij"+'\n'+"ab-anar"+'\n'+"ab-sib"+'\n'+"ab-angoor");
+    }
+    public void namesabzijat(){
+        System.out.println("reyhoon"+'\n'+"jafari"+'\n'+"piazche"+'\n'+"nanu"+'\n'+"avishan");
+    }
+    public void namemivejat(){
+        System.out.println("sib"+'\n'+"porteghal"+'\n'+"hooloo"+'\n'+"zardallo"+'\n'+"mooz"+'\n'+"gilass"+'\n'+"hendooneh");
+    }
+    public void all(){
+        System.out.println("shoyande : "+'\n'+"tayd" + '\n' + "rika " + '\n' + "mayee zarf-shoyee" + '\n' + "vaytex "
+        +'\n'+'\n'+"behdashti : "+'\n'+"shampoo"+'\n'+"saboon"+'\n'+"body splash"+'\n'+"lif"+'\n'+"dastmal-kaghazi"
+        +'\n'+'\n'+"ghazayii : "+'\n'+"makarani"+'\n'+"reshte-soop"+'\n'+"rashte-ash"+'\n'+"rob"+'\n'+"berenj"
+        +'\n'+'\n'+"nooshidani : "+'\n'+"ab-porteghal"+'\n'+"ab-havij"+'\n'+"ab-anar"+'\n'+"ab-sib"+'\n'+"ab-angoor"
+        +'\n'+'\n'+"gooshti : "+'\n'+"morgh"+'\n'+"gooosfandi"+'\n'+"gav"+'\n'+"goosale"
+        +'\n'+'\n'+"sabzijat : "+'\n'+"reyhoon"+'\n'+"jafari"+'\n'+"piazche"+'\n'+"nanu"+'\n'+"avishan"
+        +'\n'+'\n'+"mivejat : "+'\n'+"sib"+'\n'+"porteghal"+'\n'+"hooloo"+'\n'+"zardallo"+'\n'+"mooz"+'\n'+"gilass"+'\n'+"hendooneh");
+    }
+    public void gheimatsh(){
+
+        System.out.println("shoyande : "+'\n'+"tayd = 10.000" + '\n' + "rika = 15.000 " + '\n' + "mayee zarf-shoyee = 21.000" + '\n' + "vaytex = 32.000 "
+                +'\n'+'\n');
+    }
+    public void gheimatbeh(){
+        System.out.println("behdashti : "+'\n'+"shampoo = 55.000 "+'\n'+"saboon = 13.000 "+'\n'+"body splash = 80.000 "+'\n'+"lif = 20.000 "+'\n'+"dastmal-kaghazi = 25.000"
+                +'\n'+'\n');
+    }
+    public void gheimatgh(){
+        System.out.println("makarani = 15000"+'\n'+"reshte-soup = 11000"+'\n'+"rashte-ash = 13000"+'\n'+"rob = 89000"+'\n'+"berenj = 150 harkilo"+'\n'+'\n');
+    }
+    public void gheimatab(){
+        System.out.println("nooshidani : "+'\n'+"ab-porteghal = 25.000"+'\n'+"ab-havij = 28.000 "+'\n'+"ab-anar = 40.000"+'\n'+"ab-sib = 30.000"+'\n'+"ab-angoor = 32.000"
+                +'\n'+'\n');
+    }
+    public void gheimatgo(){
+        System.out.println("gooshti : "+'\n'+"morgh = 100.000"+'\n'+"gooosfandi = 200.000 yek kilo"+'\n'+"gav = 180.000 yek kilo"+'\n'+"goosale = 170.000 yek kilo"
+                +'\n'+'\n');
+    }
+    public void gheimatsabzijat(){
+        System.out.println
+                ("sabzijat : "+'\n'+"reyhoon = 12.000"+'\n'+"jafari = 9.000 "+'\n'+"piazche = 10.000"+'\n'+"nanu = 14.000"+'\n'+"avishan = 11.000"
+                +'\n'+'\n');
+    }
+    public void gheimatmive(){
+        System.out.println( "mivejat : "+'\n'+"sib = 21.000 yek kilo "+'\n'+"porteghal = 18.000 yek kilo"+'\n'+"hooloo = 25.000 yek kilo"+'\n'+
+                   "zardallo = 30.000 yek kilo"+'\n'+"mooz = 45.000 yek kilo"+'\n'+"gilass = 50.000 yek kilo"+'\n'+"hendooneh = 20.000 yek kilo");
+
+    }
+    public void  shiftname(){
+
+        System.out.println("1.sobh");
+        System.out.println("2.zohr");
+        System.out.println("3.shab");
+    }
+    }
+
