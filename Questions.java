b614728f45a5310fe8b099a0ad1077707da890fb
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-03-18 13:10:56 +0330

    Control Konsole Question

---
+++ Questions.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Questins {
+
+
+    public void modir(){
+        System.out.println("be bakhsh modir khosh umadid");
+        System.out.println("1.baray etela az mahsolat anbar shobe asli add 1 ra vared konid");
+        System.out.println("2.baray etela az  gheimat tamum mahsolat anbar shobe asli  add 2 ra vared konid");
+        System.out.println("3.baray etela az  shifhaye mojod  adad 3 ra vared konid");
+        System.out.println("4.baray ezafe kardan admin adad 4 ra vared konid");
+        System.out.println("5.baray ezafe kardan foroshgah jadid adad 5 ra vared konid");
+    }
+    public void modir2(){
+        System.out.println("1.mavad shoyande  adad 1 ra vared konid ");
+        System.out.println("2.mavad behdashti adad 2 ra vared konid ");
+        System.out.println("3.mavad ghazayii  adad 3 ra vared konid ");
+        System.out.println("4.mavad noshidani adad 4 ra vared konid ");
+        System.out.println("5.mavad gooshtii   adad 5 ra vared konid ");
+        System.out.println("6.mavad sabzijat  adad 6 ra vared konid ");
+        System.out.println("7.mavad mivejat  adad  7 ra vared konid ");
+        System.out.println("8.hame  kalaha   adad  8 ra vared konid ");
+
+    }
+    public void modir3(){
+        System.out.println("1.gheimat shoyandeh adad 1 ra vared konid ");
+        System.out.println("2.gheimat behdashti adad 2 ra vared konid ");
+        System.out.println("3.gheimat ghazaii   adad 3 ra vared konid ");
+        System.out.println("4.gheimat nooshiadani adad 4 ra vared konid ");
+        System.out.println("5.gheimat sabzijat adad 5 ra vared konid ");
+        System.out.println("6.gheimat mivejat  adad 6 ra vared konid ");
+
+    }
+    public void admin1(){
+        System.out.println(" be bakhsh admin khosh amadid :) ");
+        System.out.println("1.baray search kardan mahsolat be hamrah gheimat foroshgah adad 1 ra vared konid ");
+        System.out.println("2.baray afzodan kala adad 2 ra vared konid ");
+        System.out.println("3.baray moshahede moojodi va sharj kala adad 3 ra vared konid (csv) ");
+        System.out.println("4.baray moshahede va afzodan dastebandyha va labalha adad 4 ra vared konid ");
+        System.out.println("5.baray afzodan moshtari jadid adad 5 ra vared konid  ");
+        System.out.println("6.baray moshahede factor ha adad 6 ra vared konid ");
+      //  System.out.println("6.mikhahid dorbin baray aks gereftan roshan shavad ?(true/false) ");
+
+    }
+    public void admin2(){
+        System.out.println("kalaye jadid dar che bakhshi ezafe shavad ?");
+        System.out.println("az mavared zir adad mord nazar ra entekhub konid ");
+        System.out.println("1.shoyande");
+        System.out.println("2.behdashti");
+        System.out.println("3.ghazayii");
+        System.out.println("4.noshidani");
+        System.out.println("5.gooshti");
+        System.out.println("6.sabzijat");
+        System.out.println("7.mivejat");
+
+    }
+    public void admin3(){
+
+        System.out.println("1.shoyande ");
+        System.out.println("2.behdashti");
+        System.out.println("3.ghazayii");
+        System.out.println("4.noshidani");
+        System.out.println("5.gooshti");
+        System.out.println("6.sabzijat");
+        System.out.println("7.mivejat");
+
+    }
+
+    public void moshtari1(){
+
+        System.out.println("be bakhsh moshtariha khosh umadid");
+        System.out.println("1.moshahede tamam mahsolat va search kardan mahsolat foroshgah be hamrah gheimat adad 1 ra vared konid");
+        System.out.println("2.baray fallow kardan foroshgah adad 2 ra vared konid ");
+        System.out.println("3.baray sabtnazar darbare mahsolat foroshgah adad 3 ra vared konid");
+        System.out.println("4.baray kharid az foroshgah va daryaft factor foroosh adad 4 ra vared konid ");
+        System.out.println("5.baray moshahede mahsolat dar daste bandi adad 5 ra vared konid ");
+    }
+
+    public void moshtari2(){
+
+        System.out.println("baray moshahede mahsolat+gheimat har ghesmat adad mord nazar ra vared konid :D ");
+        System.out.println("1.behdashti");
+        System.out.println("2.shoyande");
+        System.out.println("3.ghazaii");
+        System.out.println("4.noshidani");
+        System.out.println("5.gooshti");
+        System.out.println("6.sabzijat");
+        System.out.println("7.mivejat");
+
+    }
+}
