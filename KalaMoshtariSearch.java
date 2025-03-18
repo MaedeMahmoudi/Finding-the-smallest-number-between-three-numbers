447a21041bad07a3cd5a1960bbe94844b50d1399
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-03-18 12:59:16 +0330

    printing commodity

---
+++ KalaMoshtariSearch.java
@@ -0,0 +1,62 @@
+public class KalaMoshtariSearch {
+
+    int kalam ;
+
+    public void kalamoshtari(int kalam){
+
+        switch (kalam){
+            case 1 :
+                    System.out.println("1.tayd  = 10000");
+                    System.out.println("2.rika = 15000 ");
+                    System.out.println("3.mayee zarfshoyee = 21000");
+                    System.out.println("4.vaytex = 32000 ");
+                    break;
+
+            case 2 :
+                System.out.println("1.shampoo = 55000 ");
+                System.out.println("2.saboon = 13000 ");
+                System.out.println("3.body splash = 80000 ");
+                System.out.println("4.lif = 20000 ");
+                break;
+
+            case 3 :
+                System.out.println("1.makarony = 20000");
+                System.out.println("2.reshte-soop = 16000");
+                System.out.println("3.reshte-ash = 18000");
+                System.out.println("4.rob = 90000 ");
+                break;
+
+            case 4 :
+                System.out.println("1.ab-porteghal = 25000 ");
+                System.out.println("2.ab-havij = 28000");
+                System.out.println("3.ab-anar = 40000");
+                System.out.println("4.ab-sib = 30000 ");
+                break;
+
+            case 5 :
+                System.out.println("1.morgh = 100000 ");
+                System.out.println("2.gosfandi = 200000");
+                System.out.println("3.gav = 180000");
+                System.out.println("4.goosale = 170000");
+                break;
+
+            case 6 :
+                System.out.println("1.reyhoon = 12000 ");
+                System.out.println("2.jafari = 9000 ");
+                System.out.println("3.piazche = 10000 ");
+                System.out.println("4.nanu = 14000 ");
+                System.out.println("5.avishan = 11000");
+                break;
+
+            case 7 :
+                System.out.println("1.sib = 21000 ");
+                System.out.println("2.porteghal = 18000");
+                System.out.println("3.hooloo = 25000 ");
+                System.out.println("4.zardaloo = 30000");
+                System.out.println("5.mooz = 45000 ");
+                System.out.println("6.hendooneh = 20000 ");
+                System.out.println("7.gilas = 50000");
+                break;
+        }
+    }
+}
