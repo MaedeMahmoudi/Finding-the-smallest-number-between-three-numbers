c077de0d7fbb4023bf76c17e3e12bfbca32cf392
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-03-18 13:12:00 +0330

    Time work for Admins

---
+++ Shift.java
@@ -0,0 +1,31 @@
+public class Shift {
+
+    private String sobh;
+    private String zohr;
+    private String shab;
+
+
+    public Shift() {
+    }
+
+    public String getSobh(){
+     return sobh;
+    }
+    public void setsobh(String sobh){
+        this.sobh = sobh;
+    }
+    public String getZohr(){
+        return zohr;
+    }
+    public void setZohr(String zohr){
+        this.zohr = zohr;
+    }
+    public String getShab(){
+        return shab;
+    }
+    public void setShab(String shab){
+        this.shab = shab;
+    }
+
+
+}
